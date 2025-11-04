d5b8b2a07ef7ee208ee2272705f62b3659f785cb
Nati Dogg <natnael.wondmu@gmail.com>
2025-11-04 01:36:14 -0800

    hero component got fixed

--- client/src/Components/Hero.jsx
+++ client/src/Components/Hero.jsx
@@ -19,49 +19,49 @@ const Hero = () => {
                     </button>
                 </div>
                 <div>
-                   <h1 className='text-2xl md:text-4xl font-semibold text-white capitalize'>Explore <span className='text-yellow-300'>Exceptional</span> Properites located in stunning surroundings.</h1>
+                   <h1 className='text-2xl md:text-4xl font-semibold text-white capitalize'>Explore <span className='bg-linear-to-r from-yellow-400 to-white bg-clip-text text-transparent'>Exceptional Properites </span> located in stunning surroundings.</h1>
                 </div>
                 <div className=' w-full  text-black '>
-                     <div className='bg-white px-4 py-3 rounded-md grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4 lg:gap-6 '>
+                     <form className='bg-white px-4 py-3 rounded-md grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4 lg:gap-6 '>
                          <div className='flex flex-col gap-1'>
-                           <h3 className='flex flex-row gap-1 items-center text-sm text-gray-500 '>
+                           <label htmlFor='destination' className='flex flex-row gap-1 items-center text-sm text-gray-500 '>
                                 <img src={locationIcon} alt="location icon" className='w-4 h-4' />
                                 Destination
-                           </h3>
-                           <input type="text" placeholder='Type here' className='border text-gray-700  text-sm border-gray-300 outline-none  focus:border-gray-900 rounded-sm px-2 py-1' />
+                           </label>
+                           <input id='destination' type="text" placeholder='Type here' className='border text-gray-700  text-sm border-gray-300 outline-none  focus:border-gray-900 rounded-sm px-2 py-1' />
                            
                          </div>
                          <div className='flex flex-col gap-1'>
-                           <h3 className='flex flex-row gap-1 items-center text-sm text-gray-500 '>
+                           <label htmlFor='checkin' className='flex flex-row gap-1 items-center text-sm text-gray-500 '>
                                 <img src={calenderIcon} alt="location icon" className='w-4 h-4' />
                                 Check in
-                           </h3>
-                           <input type="date"  className='border text-sm border-gray-300 text-gray-700 outline-none  focus:border-gray-900 rounded-sm px-2 py-1' />
+                           </label>
+                           <input id='checkin' type="date"  className='border text-sm border-gray-300 text-gray-700 outline-none  focus:border-gray-900 rounded-sm px-2 py-1' />
                            
                          </div>
                          <div className='flex flex-col gap-1'>
-                           <h3 className='flex flex-row gap-1 items-center text-sm text-gray-500 '>
+                           <label htmlFor='checkout' className='flex flex-row gap-1 items-center text-sm text-gray-500 '>
                                 <img src={calenderIcon} alt="location icon" className='w-4 h-4' />
                                 Check out
-                           </h3>
-                           <input type="date"  className='border text-sm border-gray-300 text-gray-700 outline-none  focus:border-gray-900 rounded-sm px-2 py-1' />
+                           </label>
+                           <input id='checkout' type="date"  className='border text-sm border-gray-300 text-gray-700 outline-none  focus:border-gray-900 rounded-sm px-2 py-1' />
                            
                          </div>
                          <div className='flex flex-col gap-1'>
-                           <h3 className='flex flex-row gap-1 items-center text-sm text-gray-500 '>
+                           <label htmlFor='guests' className='flex flex-row gap-1 items-center text-sm text-gray-500 '>
                                 <img src={usersIcon} alt="location icon" className='w-4 h-4' />
                                 Guests
-                           </h3>
-                           <input type="number" min={1}  className='border text-sm border-gray-300 text-gray-700 outline-none  focus:border-gray-900 rounded-sm px-2 py-1' />
+                           </label>
+                           <input id='guests' placeholder='1' type="number" min={1}  className='border text-sm border-gray-300 text-gray-700 outline-none  focus:border-gray-900 rounded-sm px-2 py-1' />
                            
                          </div>
-                         <div className='grid mt-0 lg:mt-3 col-span-1 md:col-span-2 lg:col-span-1'>
+                         <div className='grid mt-0 lg:mt-2 col-span-1 md:col-span-2 lg:col-span-1'>
                             <button className='bg-black flex items-center justify-center w-full  gap-1 text-[16px] cursor-pointer text-white px-4 py-2 rounded-md text-center'> 
                               <img src={searchIcon} className='invert h-4 w-4' alt="" />
                               Search
                             </button>
                          </div>
-                     </div>
+                     </form>
                 </div>
                
              </div>
